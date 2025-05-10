8f86e715350b7dd2a55732001a8d82af98bd2e41
Sergey Bryanskiy <s.brianski@yahoo.com>
2025-05-10 19:03:35 +0300

    фикс энвов дб

--- app/config/config.py
+++ app/config/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -7,6 +7,14 @@ class Settings(BaseSettings):
     db_password: str = "postgres"
     db_host: str = "localhost"
     db_port: int = 5432
+
+    model_config = SettingsConfigDict(fields={
+        "db_name": {"env": "DB_NAME"},
+        "db_user": {"env": "DB_USER"},
+        "db_password": {"env": "DB_PASSWORD"},
+        "db_host": {"env": "DB_HOST"},
+        "db_port": {"env": "DB_PORT"},
+    })
 
     @property
     def dsn(self) -> str:
