4ec1b9d421d6c5adaa52b89fc559e7bc1f85cfc0
iamlukadaniel <lukyanov.de@edu.spbstu.ru>
2025-05-10 18:49:28 +0300

    pg.py create get impl

--- app/db/messages/pg.py
+++ app/db/messages/pg.py
@@ -8,7 +8,41 @@ class Repo(Interface):
         self.cur = get_cursor()
 
     def create(self, msg: Message) -> Message:
-        return Message(content="sosal")
+        with self.cur as cur:
+            cur.execute(
+                """
+                INSERT INTO messages (dialog_id, sender, content)
+                VALUES (%s, %s, %s)
+                RETURNING id, EXTRACT(EPOCH FROM created_at)::BIGINT;
+                """,
+                (msg.dialog_id, msg.sender, msg.content),
+            )
+            msg_id, created_at_epoch = cur.fetchone()
+            msg.id = msg_id
+            msg.created_at = created_at_epoch
+        return msg
 
-    def get(self, req: SelectInput) -> list[Message]:
-        return [Message(content="sosal")]
+    def get(self, req: SelectInput) -> List[Message]:
+        with self.cur as cur:
+            cur.execute(
+                """
+                SELECT id, dialog_id, sender, content,
+                       EXTRACT(EPOCH FROM created_at)::BIGINT
+                FROM messages
+                WHERE dialog_id = %s
+                ORDER BY created_at
+                LIMIT %s OFFSET %s;
+                """,
+                (req.chat_id, req.limit, req.offset),
+            )
+            rows = cur.fetchall()
+            return [
+                Message(
+                    id=r[0],
+                    dialog_id=r[1],
+                    sender=r[2],
+                    content=r[3],
+                    created_at=r[4],
+                )
+                for r in rows
+            ]
