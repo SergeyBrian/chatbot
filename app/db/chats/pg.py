e23a4bde743caca408240934dac3a4dd4f84e87b
iamlukadaniel <lukyanov.de@edu.spbstu.ru>
2025-05-10 22:32:49 +0300

    chats pg.py impl

--- app/db/chats/pg.py
+++ app/db/chats/pg.py
@@ -5,15 +5,99 @@ from app.model.chat import Chat
 
 class Repo(Interface):
     def __init__(self):
+        # контекстный менеджер для курсора и коммита
         self.cur = get_cursor
 
     def create(self, msg: Chat) -> Chat:
-        ...
+        with self.cur() as cur:
+            cur.execute(
+                """
+                INSERT INTO chats (user_id, status)
+                VALUES (%s, %s)
+                RETURNING
+                  id,
+                  user_id,
+                  status,
+                  EXTRACT(EPOCH FROM started_at)::BIGINT,
+                  EXTRACT(EPOCH FROM COALESCE(closed_at, to_timestamp(0)))::BIGINT;
+                """,
+                (msg.user_id, msg.status),
+            )
+            row = cur.fetchone()
+            return Chat(
+                id=row[0],
+                user_id=row[1],
+                status=row[2],
+                started_at=row[3],
+                closed_at=row[4],
+            )
 
     def get(self, req: SelectInput) -> list[Chat]:
-        ...
+        with self.cur() as cur:
+            cur.execute(
+                """
+                SELECT
+                  id,
+                  user_id,
+                  status,
+                  EXTRACT(EPOCH FROM started_at)::BIGINT,
+                  EXTRACT(EPOCH FROM COALESCE(closed_at, to_timestamp(0)))::BIGINT
+                FROM chats
+                ORDER BY started_at
+                LIMIT %s OFFSET %s;
+                """,
+                (req.limit, req.offset),
+            )
+            rows = cur.fetchall()
+            return [
+                Chat(
+                    id=r[0],
+                    user_id=r[1],
+                    status=r[2],
+                    started_at=r[3],
+                    closed_at=r[4],
+                )
+                for r in rows
+            ]
 
-    # TODO здесь нужно будет если статус closed,
-    # то также обновлять closed_at = текущий timestamp
-    def update(selef, chat: UpdateInput) -> Chat:
-        ...
+    def update(self, chat: UpdateInput) -> Chat:
+        with self.cur() as cur:
+            if chat.status == "closed":
+                cur.execute(
+                    """
+                    UPDATE chats
+                    SET status    = %s,
+                        closed_at = CURRENT_TIMESTAMP
+                    WHERE id = %s
+                    RETURNING
+                      id,
+                      user_id,
+                      status,
+                      EXTRACT(EPOCH FROM started_at)::BIGINT,
+                      EXTRACT(EPOCH FROM COALESCE(closed_at, to_timestamp(0)))::BIGINT;
+                    """,
+                    (chat.status, chat.id),
+                )
+            else:
+                cur.execute(
+                    """
+                    UPDATE chats
+                    SET status = %s
+                    WHERE id = %s
+                    RETURNING
+                      id,
+                      user_id,
+                      status,
+                      EXTRACT(EPOCH FROM started_at)::BIGINT,
+                      EXTRACT(EPOCH FROM COALESCE(closed_at, to_timestamp(0)))::BIGINT;
+                    """,
+                    (chat.status, chat.id),
+                )
+            row = cur.fetchone()
+            return Chat(
+                id=row[0],
+                user_id=row[1],
+                status=row[2],
+                started_at=row[3],
+                closed_at=row[4],
+            )
