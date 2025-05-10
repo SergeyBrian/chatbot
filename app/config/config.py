f737c55c1c04df28c63eafc1c7fda25c6ad9f413
Sergey Bryanskiy <s.brianski@yahoo.com>
2025-05-10 16:43:31 +0300

    docker fix

--- app/config/config.py
+++ app/config/config.py
@@ -2,15 +2,15 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    db_name: str = "yourdb"
-    db_user: str = "youruser"
-    db_password: str = "yourpassword"
+    db_name: str = "db"
+    db_user: str = "postgres"
+    db_password: str = "postgres"
     db_host: str = "localhost"
     db_port: int = 5432
 
     @property
     def dsn(self) -> str:
-        return f"dbname={self.db_name} user={self.db_user} password={self.db_password} host={self.db_host} port={self.db_port}"
+        return f"dbname={self.db_name} user={self.db_user} password={self.db_password} host={self.db_host} port={self.db_port} sslmode=disable"
 
 
 settings = Settings()
